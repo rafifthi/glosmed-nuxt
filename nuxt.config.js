8899f02f70e70ff60e658b8e7e3a647697790fdb
rafifthi <rafiffathii@gmail.com>
2022-09-13 18:20:09 +0700

    Font fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,10 @@ export default {
       {
         rel: 'stylesheet',
         href: 'https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap'
       }
     ]
   },
